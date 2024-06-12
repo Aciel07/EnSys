081f6fa996f3e441be34fac8d79bbac37c97c9a9
Aciel <106684053+Aciel07@users.noreply.github.com>
2024-06-12 14:10:13 +0800

    Update main.py

--- EnSys-main/main.py
+++ EnSys-main/main.py
@@ -2,9 +2,17 @@
 This is the main file for the EnSys chatbot. It initializes the Flask app and loads the chatbot with the API key.
 Old model: ft:gpt-3.5-turbo-0125:ensys:restaurant:9IrHirrM 
 '''
+import nltk
+import os
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+nltk.download('wordnet')
+
 from flask import Flask
 from routes import initialize_routes
 from bot import EnSysBot, load_config
+
 
 # Load environment variables and get the API key
 try:
@@ -22,4 +30,5 @@ app = Flask(__name__)
 initialize_routes(app, bot)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
